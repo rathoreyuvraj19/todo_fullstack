82464c096f20420f7d279dee122f069efdd4803e
Yuvraj Singh Rathore <rathore.yuvraj19@gmail.com>
2023-11-05 16:53:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import bodyParser from "body-parser";
+import express from "express";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Todo",
+  password: "12345",
+  port: 5432,
+});
+
+db.connect();
+
+let items = [];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM tasks ORDER BY id ASC");
+  items = result.rows;
+  // console.log(items);
+  res.render("index.ejs", { items: items });
+});
+
+app.post("/add", async (req, res) => {
+  // console.log(req.body.newTask);
+  await db.query("INSERT INTO tasks (task) VALUES ($1)", [req.body.newTask]);
+  res.redirect("/");
+});
+
+app.post("/edit", async (req, res) => {
+  // console.log(req.body);
+  await db.query("UPDATE tasks SET task=$1 WHERE id=$2", [
+    req.body.edit,
+    req.body.id,
+  ]);
+  res.redirect("/");
+});
+
+app.post("/done", async (req, res) => {
+  const result = items.find((item) => item.id == req.body.taskIdreal);
+  // console.log(req.body);
+  await db.query("UPDATE tasks SET condition=$1 WHERE id=$2", [
+    !result.condition,
+    req.body.taskIdreal,
+  ]);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
